8f4848af8f3430e25974a2fa8ee5ddf5a8556a40
Neville <nevillegilor@gmail.com>
2025-03-06 11:42:33 +0100

    Update listas_ej2.pas

--- lista-simple/listas_ej2.pas
+++ lista-simple/listas_ej2.pas
@@ -19,18 +19,35 @@ if not is_empty(lista) then
     writeln('Diferencia: ', last(lista) - first(lista));
 
     // 2.4 Número de elementos, eliminar al inicio, limpiar lista
+writeln('Número de elementos: ', num_elems(lista));
+delete_at_begin(lista);
+clear(lista);
 
     // 2.5 Verificar si la lista está vacía, copiar lista, eliminar al inicio
+if is_empty(lista) then
+    writeln('La lista está vacía.')
+else
+begin
+    copy(lista, lista_copia);
+    delete_at_begin(lista_copia);
+end;
 
     // 2.6 Insertar al inicio
+insert_at_begin(lista, 5);
 
     // 2.7 Eliminar al final;
+delete_at_end(lista);
 
     // 2.8 Eliminar un elemento específico
+delete(lista, 20);
 
     // 2.9 Verificar si un elemento está en la lista
+if in_list(lista, 10) then
+    writeln('El elemento 10 está en la lista.');
 
     // 2.10 Verificar recursivamente si un elemento está en la lista
+if rec_in_list(lista, 15) then
+    writeln('El elemento 15 está en la lista (verificación recursiva).');
 
 
     readln;
